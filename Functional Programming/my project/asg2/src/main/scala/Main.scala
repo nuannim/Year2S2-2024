bb5e848f1a7a3fa4094b4bbc41e5cf1045f8b9bd
nuannim <66070286@kmitl.ac.th>
2025-01-17 14:22:51 +0700

    fp : assignment02

---
+++ Functional Programming/my project/asg2/src/main/scala/Main.scala
@@ -0,0 +1,56 @@
+
+
+def tail[T](seq: Seq[T]): Seq[T] = seq match {
+  case Nil => Nil
+  case _ :: rest => rest
+}
+
+def drop[T](seq: Seq[T], num: Int): Seq[T] = {
+  if (num == 0) seq
+  else drop(tail(seq), num - 1)
+}
+
+def dropWhile[T](seq: Seq[T], cond: T => Boolean): Seq[T] = seq match {
+  case Nil => seq
+  case x :: rest if cond(x) => dropWhile(rest, cond)
+  case _ => seq
+}
+
+@main def main(): Unit = {
+  val exampleList = Seq("I", "LOVE", "Andaman", "of", "Thailand")
+
+  val tailResult = tail(exampleList)
+  println(s"Tail Result : $tailResult")
+
+  val dropResult = drop(exampleList, 2)
+  println(s"Drop Result : $dropResult")
+
+  val dropWhileResult = dropWhile(exampleList, (x: String) => x != "of")
+  println(s"DropWhile Result : $dropWhileResult")
+
+// /////////////////////////////////////////////////////////////////////////
+  val list2 = Seq("Good", "Morning", "everyone")
+
+  val tail2 = tail(list2)
+  println(s"Tail Result : $tail2")
+
+  val drop2 = drop(list2, 2)
+  println(s"Drop Result : $drop2")
+
+  val dropWhile2 = dropWhile(list2, (x: String) => x != "of")
+  println(s"DropWhile Result : $dropWhile2")
+
+// /////////////////////////////////////////////////////////////////////////
+  val list3 = Seq("A", "B", "C", "D")
+
+  val tail3 = tail(list3)
+  println(s"Tail Result : $tail3")
+
+  val drop3 = drop(list3, 2)
+  println(s"Drop Result : $drop3")
+
+  val dropWhile3 = dropWhile(list3, (x: String) => x != "of")
+  println(s"DropWhile Result : $dropWhile3")
+
+}
+
