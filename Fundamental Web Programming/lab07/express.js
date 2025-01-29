8feab825e960f42fc1f7dfea9d13a2d2f523d783
nuannim <66070286@kmitl.ac.th>
2025-01-29 14:41:34 +0700

    fwp : lab7 all done

--- Fundamental Web Programming/lab07/express.js
+++ Fundamental Web Programming/lab07/express.js
@@ -1,27 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// * ใครไม่รู้บอกให้ทำงี้ แต่เหมือนจะผิด ขอยาดโละ ย้ายไป index.js
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-const path = require('path');
+// const path = require('path');
 
-// create directory 'public'
-app.use(express.static('public'));
-app.use(express.static('images'));
+// // create directory 'public'
+// app.use(express.static('public'));
+// app.use(express.static('images'));
 
-// Without middleware
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/home.html'));
-});
+// // Without middleware
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '/public/home.html'));
+// });
 
-app.get('/hello', function(req, res){
-  res.send("Hello World!");
-});
+// app.get('/hello', function(req, res){
+//   res.send("Hello World!");
+// });
 
-app.post('/hello', function(req, res){
-  res.send("You just called the post method at '/hello'!\n");
-});
+// app.post('/hello', function(req, res){
+//   res.send("You just called the post method at '/hello'!\n");
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`)
+// })
 
--- Fundamental Web Programming/lab07/index.js
+++ Fundamental Web Programming/lab07/index.js
@@ -1,40 +1,85 @@
-const http = require('http') 
-const port = process.env.PORT || 3000 
+//* ไม่ได้ใช้ express
+// const http = require('http') 
+// const port = process.env.PORT || 3000 
 
-const fs = require('fs') 
+// const fs = require('fs') 
 
-function serveStaticFile(res, path, contentType,
-    responseCode = 200) {
-    fs.readFile(__dirname + path, (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' })
-            return res.end('500 - Internal Error')
-        }
-        res.writeHead(responseCode, {
-            'Content-Type':
-                contentType
-        })
-        res.end(data)
-    })
-} 
+// function serveStaticFile(res, path, contentType,
+//     responseCode = 200) {
+//     fs.readFile(__dirname + path, (err, data) => {
+//         if (err) {
+//             res.writeHead(500, { 'Content-Type': 'text/plain' })
+//             return res.end('500 - Internal Error')
+//         }
+//         res.writeHead(responseCode, {
+//             'Content-Type':
+//                 contentType
+//         })
+//         res.end(data)
+//     })
+// } 
  
-const server = http.createServer((req,res) => {
-    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
-    switch(path) {
-      case '':
-        serveStaticFile(res, '/public/home.html', 'text/html')
-        break
-      case '/about':
-        serveStaticFile(res, '/public/about.html', 'text/html')
-        break
-      case '/img/panda-logo.png':
-        serveStaticFile(res, '/public/img/panda-logo.png', 'image/png')
-        break
-      default:
-        serveStaticFile(res, '/public/404.html', 'text/html', 404)
-        break
-    }
-  })
+// const server = http.createServer((req,res) => {
+//     const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+//     switch(path) {
+//       case '':
+//         serveStaticFile(res, '/public/home.html', 'text/html')
+//         break
+//       case '/about':
+//         serveStaticFile(res, '/public/about.html', 'text/html')
+//         break
+//       case '/img/panda-logo.png':
+//         serveStaticFile(res, '/public/img/panda-logo.png', 'image/png')
+//         break
+//       default:
+//         serveStaticFile(res, '/public/404.html', 'text/html', 404)
+//         break
+//     }
+//   })
  
-server.listen(port, () => console.log(`server started on 
-port ${port}; ` + 'press Ctrl-C to terminate....'))
+// server.listen(port, () => console.log(`server started on 
+// port ${port}; ` + 'press Ctrl-C to terminate....'))
+
+// * ใช้ express
+const express = require('express') //* import express
+const app = express()
+const port = 3000
+
+app.get('/hello', function(req, res){
+  res.send("Hello World!");
+});
+
+app.post('/hello', function(req, res){
+  res.send("You just called the post method at '/hello'!\n");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
+const path = require('path'); //* import path
+
+// create directory 'public'
+app.use(express.static('public'));
+app.use(express.static('images'));
+
+// Without middleware
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/home.html'));
+});
+
+app.get('/cat', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/cat.html'));
+});
+
+app.get('/dog', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/dog.html'));
+});
+
+app.get('/bird', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/bird.html'));
+});
+
+app.get('/about', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/about.html'));
+});
